e0c09b8cf2e3e417cb890b25d934b7e39775b50f
petruk1 <vpetruk.osn@gmail.com>
2018-12-14 16:51:37 +0200

    remove unused google types

--- src/app/google-types.ts
+++ src/app/google-types.ts
@@ -6,14 +6,6 @@ export class Marker extends google.maps.Marker {
 
 }
 
-export class LatLng extends google.maps.LatLng {
-
-}
-
-export class Geocoder extends google.maps.Geocoder {
-
-}
-
 export class PlaceService extends google.maps.places.PlacesService {
 
 }
@@ -26,11 +18,12 @@ export class MapPoint extends google.maps.Point {
 }
 
 import PlacesServiceStatus = google.maps.places.PlacesServiceStatus;
+import MapEvent = google.maps.event;
 
 export {PlacesServiceStatus};
 
 export interface PlaceResult extends google.maps.places.PlaceResult {
 
 }
-import MapEvent = google.maps.event;
+
 export {MapEvent};
